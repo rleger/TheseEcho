7e04cec6030dc8c0df65d8dc3e001d07bfed12ae
Romain Léger <legerrom@gmail.com>
2015-07-20 16:09:36 +0200

    tweaking

--- resources/views/survey/survey_header.blade.php
+++ resources/views/survey/survey_header.blade.php
@@ -1,13 +1,13 @@
 <!-- Header -->
 <div class="survey-header">
-    <div class="fullscreen background" style="background-image:url('../../images/echographie-bck.jpg');" data-img-width="1600" data-img-height="1188">
+    <div class="fullscreen background" style="background-image:url('../../images/echographie-background-blur.jpg');" data-img-width="1600" data-img-height="1188">
         <div class="content-a">
             <div class="content-b">
                 <div class="content-c">
                     {!! HTML::image('images/icons/chat.png', null, ["width" => 100, "height" => 100]) !!}
 
                     <h3 class='survey-title'>Echographie en médecine générale</h3>
-                    <h6 class='survey-subtitle'>SOUS TITRE A CHANGER</h6>
+                    <h6 class='survey-subtitle'>Utilisation et intérêts en cabinet de médecine générale<br>Enquête comparative France / Suisse</h6>
                     <p><a class="btn start-btn" href="#start" role="button">Débuter</a>
                     </p>
                 </div>
